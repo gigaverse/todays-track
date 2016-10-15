49697ba3dd0bcab7c432ee85225c61dc47740992
Youssef Mahmoud <youssefpower1997@gmail.com>
2016-10-15 09:13:39 -0500

    Fixed FFmpeg

--- app/src/main/java/yambritton/hackrice/todays_track/testing.java
+++ app/src/main/java/yambritton/hackrice/todays_track/testing.java
@@ -2,6 +2,7 @@ package yambritton.hackrice.todays_track;
 
 import android.Manifest;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
@@ -10,8 +11,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler;
+import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
 
 public class testing extends AppCompatActivity {
 
@@ -57,4 +63,59 @@ public class testing extends AppCompatActivity {
         });
     }
 
+    public static void splitAudio(String audio, int time, Context context)
+    {
+        FFmpeg ffmpeg = FFmpeg.getInstance(context);
+        String[] cmd = String.format("-ss 10 -t 6 -i %s output.mp3", audio).split(" ");
+        try {
+            ffmpeg.execute(cmd, new ExecuteBinaryResponseHandler() {
+
+                @Override
+                public void onStart() {}
+
+                @Override
+                public void onProgress(String message) {}
+
+                @Override
+                public void onFailure(String message) {}
+
+                @Override
+                public void onSuccess(String message) {}
+
+                @Override
+                public void onFinish() {}
+            });
+        } catch (FFmpegCommandAlreadyRunningException f)
+        {
+            Log.d("FAILURE", "audio trim fuckup");
+        }
+    }
+
+    public static void mergeAudio(String video, String audio, Context context)
+    {
+        FFmpeg ffmpeg = FFmpeg.getInstance(context);
+        String[] cmd = String.format("-i %s -i %s -c:v copy -c:a copy output.mp4", video, audio).split(" ");
+        try {
+            ffmpeg.execute(cmd, new ExecuteBinaryResponseHandler() {
+
+                @Override
+                public void onStart() {}
+
+                @Override
+                public void onProgress(String message) {}
+
+                @Override
+                public void onFailure(String message) {}
+
+                @Override
+                public void onSuccess(String message) {}
+
+                @Override
+                public void onFinish() {}
+            });
+        } catch (FFmpegCommandAlreadyRunningException f)
+        {
+            Log.d("FAILURE", "audio video merge fuckup");
+        }
+    }
 }
