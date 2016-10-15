5e7924a869370a0a64f9e0952099f4abe9863845
gigaverse <youssefpower1997@gmail.com>
2016-10-15 17:26:38 -0500

    Got FFmpeg to read files

--- app/src/main/java/yambritton/hackrice/todays_track/testing.java
+++ app/src/main/java/yambritton/hackrice/todays_track/testing.java
@@ -6,11 +6,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -20,21 +24,54 @@ import android.widget.VideoView;
 
 import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler;
 import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
+import com.github.hiteshsondhi88.libffmpeg.LoadBinaryResponseHandler;
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class testing extends AppCompatActivity {
 
+    int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE;
+    String videoURI = "", audioURI = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testing);
+        Log.d("inputstring", Environment.getExternalStorageDirectory().getAbsolutePath());
+        FFmpegInit();
+
+        //GETTING PERMISSION FOR EVERYTHING
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
 
         final Button videobutton = (Button) findViewById(R.id.videobutton);
 
         videobutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //  select video
-                pickMedia((VideoView) findViewById(R.id.videoView));
+                pickVideo();
 
             }
         });
@@ -43,22 +80,34 @@ public class testing extends AppCompatActivity {
 
         audiobutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                pickMedia();
             }
         });
 
         final Button outputbutton = (Button) findViewById(R.id.outputbutton);
-
         outputbutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // generate output
+                try {
+                    mergeAudio(videoURI, audioURI);
+                }
+                catch (Exception e)
+                {
+                    Log.d("inputstring", e.toString());
+                    e.printStackTrace();
+                }
             }
         });
     }
 
-    public void pickMedia(View view){
+    public void pickVideo(){
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent , 1);
+    }
+
+    public void pickMedia(){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent , 2);
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -68,18 +117,31 @@ public class testing extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // The user picked a contact.
                 // The Intent's data Uri identifies which contact was selected.
-                Log.i("data", data.getData().toString());
+                videoURI = getPath(data.getData()).replaceAll("[ ]+", "\\ ");
+                Log.i("inputstring", videoURI);
                 VideoView video = (VideoView) findViewById(R.id.videoView);
                 video.setVideoURI(data.getData());
                 video.start();
                 // Do something with the contact here (bigger example below)
             }
         }
+        if (requestCode == 2) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // The user picked a contact.
+                // The Intent's data Uri identifies which contact was selected.
+                audioURI = getPath(data.getData()).replaceAll("[ ]+", "\\ ");
+                Log.i("inputstring", audioURI);
+                MediaPlayer mp = MediaPlayer.create(this, data.getData());
+                mp.start();
+                // Do something with the contact here (bigger example below)
+            }
+        }
     }
 
-    public static void splitAudio(String audio, int time, Context context)
+    protected void splitAudio(String audio, int time)
     {
-        FFmpeg ffmpeg = FFmpeg.getInstance(context);
+        FFmpeg ffmpeg = FFmpeg.getInstance(this);
         String[] cmd = String.format("-ss 10 -t 6 -i %s output.mp3", audio).split(" ");
         try {
             ffmpeg.execute(cmd, new ExecuteBinaryResponseHandler() {
@@ -105,10 +167,15 @@ public class testing extends AppCompatActivity {
         }
     }
 
-    public static void mergeAudio(String video, String audio, Context context)
+    protected void mergeAudio(String video, String audio) throws IOException
     {
-        FFmpeg ffmpeg = FFmpeg.getInstance(context);
-        String[] cmd = String.format("-i %s -i %s -c:v copy -c:a copy output.mp4", video, audio).split(" ");
+        FFmpeg ffmpeg = FFmpeg.getInstance(this);
+        String[] cmd = "-i %s -i %s -c:v copy -c:a copy storage/emulated/0/todaytrack/output.mp4".split(" ");
+        File file = new File(this.getExternalFilesDir(), "");
+        Log.d("inputstring", file.getAbsolutePath());
+        cmd[1] = video;
+        cmd[3] = audio;
+        Log.d("inputstring", Arrays.toString(cmd));
         try {
             ffmpeg.execute(cmd, new ExecuteBinaryResponseHandler() {
 
@@ -119,17 +186,57 @@ public class testing extends AppCompatActivity {
                 public void onProgress(String message) {}
 
                 @Override
-                public void onFailure(String message) {}
+                public void onFailure(String message) {
+                    Log.d("inputstring", message);
+                }
 
                 @Override
-                public void onSuccess(String message) {}
+                public void onSuccess(String message) {
+                    Log.d("inputstring", message);
+                }
 
                 @Override
-                public void onFinish() {}
+                public void onFinish() {
+                    Log.d("inputstring", "close");
+                }
             });
         } catch (FFmpegCommandAlreadyRunningException f)
         {
             Log.d("FAILURE", "audio video merge fuckup");
         }
     }
+
+    protected void FFmpegInit()
+    {
+        FFmpeg ffmpeg = FFmpeg.getInstance(this);
+        try {
+            ffmpeg.loadBinary(new LoadBinaryResponseHandler() {
+
+                @Override
+                public void onStart() {}
+
+                @Override
+                public void onFailure() {}
+
+                @Override
+                public void onSuccess() {}
+
+                @Override
+                public void onFinish() {}
+            });
+        } catch (FFmpegNotSupportedException e) {
+            // Handle if FFmpeg is not supported by device
+        }
+
+    }
+
+    public String getPath(Uri contentUri)
+    {
+        String[] proj = { MediaStore.Audio.Media.DATA };
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null); //Since manageQuery is deprecated
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+
 }
