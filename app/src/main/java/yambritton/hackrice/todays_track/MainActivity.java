4f8c8a2d836aee0dde1c71e22643157629c58138
7e5h <sambritton212@gmail.com>
2016-10-15 08:39:34 -0500

    video selection

--- app/src/main/java/yambritton/hackrice/todays_track/MainActivity.java
+++ app/src/main/java/yambritton/hackrice/todays_track/MainActivity.java
@@ -1,7 +1,12 @@
 package yambritton.hackrice.todays_track;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void pickMedia(View view){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent , 1);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == 1) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // The user picked a contact.
+                // The Intent's data Uri identifies which contact was selected.
+                Log.i("data",data.getData().toString());
+                VideoView video = (VideoView)findViewById(R.id.videoView);
+                video.setVideoURI(data.getData());
+                video.start();
+
+                // Do something with the contact here (bigger example below)
+            }
+        }
     }
 }
