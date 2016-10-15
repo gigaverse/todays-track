eb7ecd99dedffa474fb874e2056e37b5ff7b09da
Youssef Mahmoud <youssefpower1997@gmail.com>
2016-10-14 23:14:51 -0400

    Initial generation
    -Generated the project
    -Added FFMPEG library

---
+++ app/src/main/java/yambritton/hackrice/todays_track/MainActivity.java
@@ -0,0 +1,13 @@
+package yambritton.hackrice.todays_track;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
